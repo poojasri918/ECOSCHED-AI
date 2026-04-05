07ac9ce03a4c6afe9ea347a1da7feb14561382ba
pooja <poojasri09018@gmail.com>
2026-04-05 12:29:53 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -721,7 +721,7 @@ async function runSim(){
 
 # ---------------- RUN ----------------
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
     main()
